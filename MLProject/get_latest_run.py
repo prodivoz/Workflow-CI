a8e66a04118b8f60883f819b5e1679839b9edda9
prodivoz <161691517+prodivoz@users.noreply.github.com>
2025-06-19 13:57:44 +0700

    Update get_latest_run.py

--- MLProject/get_latest_run.py
+++ MLProject/get_latest_run.py
@@ -1,21 +1,19 @@
 import mlflow
 
-# Ambil ID eksperimen berdasarkan nama
+# Ambil experiment dan run terakhir
 experiment = mlflow.get_experiment_by_name("Model ML Eksperimen")
 if experiment is None:
-    raise ValueError("Experiment 'Model ML Eksperimen' tidak ditemukan.")
-experiment_id = experiment.experiment_id
+    raise ValueError("Experiment tidak ditemukan.")
 
-# Ambil run_id terakhir dari eksperimen
+experiment_id = experiment.experiment_id
 runs = mlflow.search_runs(
     experiment_ids=[experiment_id],
     order_by=["start_time DESC"],
     max_results=1
 )
 if runs.empty:
-    raise ValueError("Tidak ada run ditemukan di experiment ini.")
-run_id = runs.iloc[0]["run_id"]
+    raise ValueError("Tidak ada run ditemukan.")
 
-# Cetak output untuk GitHub Actions
+run_id = runs.iloc[0]["run_id"]
 print(f"experiment_id: {experiment_id}")
 print(f"run_id: {run_id}")
